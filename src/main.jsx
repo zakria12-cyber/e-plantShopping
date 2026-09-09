1431e9c87f7468046043ba8f33bed5acfc7460c3
zakria12-cyber <zakii.developerncoders@gmail.com>
2026-09-09 18:40:36 +0500

    Remove old Redux provider for Foodie app

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./store";
 import App from "./App";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
